687710254f1e6e36c36745c02a1d8d99adb1aaf0
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-06-16 15:28:25 -0400

    Edit form base

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import ItemCreate from "./components/pages/item_create/ItemCreate";
 import ItemDetail from "./components/pages/item_detail/ItemDetail";
+import ItemEdit from "./components/pages/item_edit/ItemEdit";
 import ItemList from "./components/pages/item_list/ItemList";
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<ItemList />} />
-          <Route path="/item/edit/:id" element={<div>Edit</div>} />
+          <Route path="/item/edit/:id" element={<ItemEdit />} />
           <Route path="/item/:id" element={<ItemDetail />} />
           <Route path="/item/create/" element={<ItemCreate />} />
         </Routes>
---
+++ client/src/components/pages/item_edit/ItemEdit.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useParams, useNavigate } from "react-router";
+import "./ItemEdit.css";
+import {
+  Product as ProductInteface,
+  ProductPOST,
+  Category,
+} from "../../../types";
+
+type Inputs = {
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  stock: number;
+  img: string;
+};
+
+const ItemEdit = () => {
+  const [item, setItem] = useState<ProductInteface>({
+    _id: "",
+    name: "",
+    description: "",
+    category: {
+      _id: "",
+      name: "",
+    },
+    price: 0,
+    stock: 0,
+    img: "",
+  });
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    getItemToEdit();
+    getCategories();
+    updateValues();
+  }, []);
+
+  useEffect(() => {
+    updateValues();
+  }, [item]);
+
+  const params = useParams();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const getCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/categories");
+      const data = await response.json();
+      setCategories(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getItemToEdit = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/item/${params.id}`);
+      const data = await response.json();
+      console.log(data);
+      setItem(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateValues = () => {
+    setValue("name", item.name);
+    setValue("description", item.description);
+    setValue("category", item.category._id);
+    setValue("price", item.price);
+    setValue("stock", item.stock);
+    setValue("img", item.img);
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/category/${data.category}`
+      );
+      const category: Category = await response.json();
+      const newItem: ProductPOST = { ...data, category: category };
+
+      await fetch("http://localhost:4000/item/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      });
+
+      //setForm({ name: "", position: "", level: "" });
+      //navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(watch("name"));
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="item-create-form">
+      {/* register your input into the hook by invoking the "register" function */}
+      <label htmlFor="name">Item Name</label>
+      <input {...register("name", { required: true, value: item.name })} />
+      {errors.name && <span>Name is required</span>}
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <label>Description</label>
+      <textarea {...register("description", { required: true })} />
+      {errors.description && <span>Name is required</span>}
+
+      <label>Category</label>
+      <select {...register("category", { required: true })}>
+        {categories.map((category) => (
+          <option value={category._id} key={category._id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+
+      <label>Price</label>
+      <input {...register("price", { required: true, min: 0 })} />
+      {errors.description && <span>Price is required</span>}
+
+      <label>Stock</label>
+      <input {...register("stock", { required: true, min: 0 })} />
+      {errors.description && <span>Stock is required</span>}
+
+      <label>Img(URL)</label>
+      <input {...register("img", { required: false })} />
+      {errors.description && <span>Stock is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default ItemEdit;
