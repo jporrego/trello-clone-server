64a85ff78e0be72b943bb72ed291fdc5f4a88f14
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-27 18:13:44 -0400

    Changed board_lists controller to return all cards as well

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -18,12 +18,35 @@ exports.board_detail = async (req, res, next) => {
 };
 
 exports.board_lists = async (req, res, next) => {
+  /// Taking the boardId, get all the lists. Then for each list get its cards.
+  // This is done this way to get all the info in one request.
+
   try {
     const { boardId } = req.params;
+
+    const listsAndCards = {
+      lists: [],
+      cards: [],
+    };
+
     const { rows } = await db.query("SELECT * FROM list WHERE board_id = $1", [
       boardId,
     ]);
-    console.log(rows);
+
+    listsAndCards.lists = rows;
+
+    for (const list of rows) {
+      const query = await db.query("SELECT * FROM card WHERE list_id = $1", [
+        list.id,
+      ]);
+      const cards = query.rows;
+      if (cards.length > 0) {
+        for (const card of cards) {
+          listsAndCards.cards.push(card);
+        }
+      }
+    }
+    console.log(listsAndCards);
     res.status(200).json(rows);
   } catch (err) {
     console.log(err);
