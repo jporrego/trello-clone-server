681a764b7de2235f2cf1bb57658d484ce05c23e8
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-30 19:08:39 -0400

    addList

--- controllers/listController.js
+++ controllers/listController.js
@@ -19,3 +19,28 @@ exports.list_cards = async (req, res, next) => {
     res.send(500);
   }
 };
+
+// POST | Add list
+exports.add_list = async (req, res, next) => {
+  try {
+    const { board_id, listName } = req.body;
+    await db.query("INSERT INTO list (board_id, name) VALUES ($1, $2)", [
+      board_id,
+      listName,
+    ]);
+    /*
+    const queryNewId = await db.query("SELECT currval('card_id_seq')");
+    const newCardId = queryNewId.rows[0].currval;
+
+    const queryNewCard = await db.query("SELECT * FROM card WHERE id = $1", [
+      newCardId,
+    ]);
+    const newCard = queryNewCard.rows;
+
+    console.log(newCard);*/
+    console.log(1);
+    res.sendStatus(200);
+  } catch (error) {
+    res.send(500);
+  }
+};
--- routes/lists.js
+++ routes/lists.js
@@ -3,13 +3,13 @@ let router = express.Router();
 
 let lists_controller = require("../controllers/listController");
 
+// Add one list.
+router.post("/", lists_controller.add_list);
+
 // GET All cards for a board.
 router.get("/:listId/cards", lists_controller.list_cards);
 
 // GET All lists.
 router.get("/", lists_controller.lists);
-
-// Add one list.
-//router.get("/", lists_controller.add_list);
 
 module.exports = router;
