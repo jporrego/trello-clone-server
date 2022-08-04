196764e2e3faee6ccdebb056cf7e9af1d12d55eb
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-08-04 16:10:41 -0400

    get board list order working

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -28,6 +28,41 @@ exports.board_lists = async (req, res, next) => {
   }
 };
 
+exports.board_lists_order = async (req, res, next) => {
+  try {
+    const { boardId } = req.params;
+    let listsOrder = await db.query(
+      "SELECT list_order FROM board_lists_order WHERE board_id = $1",
+      [boardId]
+    );
+    if (listsOrder.rows[0] !== undefined) {
+      listsOrder = listsOrder.rows[0].list_order;
+      res.status(200).json(listsOrder);
+    } else {
+      res.sendStatus(200);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(500);
+  }
+};
+
+exports.update_board_lists_order = async (req, res, next) => {
+  try {
+    const { listId } = req.params;
+    const { card_order } = req.body;
+    console.log(listId, card_order);
+    await db.query(
+      "UPDATE list_cards_order SET cards_order = $1 WHERE list_id = $2",
+      [card_order, listId]
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.send(500);
+  }
+};
+
 exports.OUTDATED_board_lists_and_cards = async (req, res, next) => {
   /// Taking the boardId, get all the lists. Then for each list get its cards.
   // This is done this way to get all the info in one request.
--- routes/boards.js
+++ routes/boards.js
@@ -12,4 +12,10 @@ router.get("/:boardId/lists", boards_controller.board_lists);
 // GET Single board.
 router.get("/:boardId", boards_controller.board_detail);
 
+// GET list order for a board.
+router.get("/:boardId/lists/order", boards_controller.board_lists_order);
+
+// PUT update list order.
+router.put("/:boardId/lists/order", boards_controller.update_board_lists_order);
+
 module.exports = router;
