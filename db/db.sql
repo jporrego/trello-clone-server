a5fb5df6fb748f9bad44341a212f4f1f4fbb9359
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-10-24 15:26:29 -0300

    Board table redesign

--- db/db.sql
+++ db/db.sql
@@ -11,6 +11,9 @@ CREATE TABLE board
 (
   "id" SERIAL PRIMARY KEY,
   "user_id" INT NOT NULL,
+  "title" VARCHAR(50),
+  "background_color" VARCHAR(6),
+  "background_img" VARCHAR(200),
   "name" VARCHAR(100)  NOT NULL
 );
 
